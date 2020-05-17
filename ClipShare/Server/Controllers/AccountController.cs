a9bd903aca6dd2f50b1318a4b1de1eab1b465617
Jared Goodwin <translucency@outlook.com>
2020-05-17 11:11:56 -0700

    Add project files.

---
+++ ClipShare/Server/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClipShare.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public bool IsLoggedIn()
+        {
+            return User.Identity.IsAuthenticated;
+        }
+    }
+}
