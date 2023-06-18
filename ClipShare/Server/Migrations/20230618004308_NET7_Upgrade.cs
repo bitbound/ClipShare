5087513f1a321f5a9a16d303acb3278ccaf50d85
Jared Goodwin <translucency@outlook.com>
2023-06-17 17:49:41 -0700

    Add migrations.  Clean up CSS.

---
+++ ClipShare/Server/Migrations/20230618004308_NET7_Upgrade.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClipShare.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class NET7_Upgrade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
